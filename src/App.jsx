d1649133c25ef5a61ea4cbbd7ca38fa9e4430930
affan-anan <sarkeraffan@gmail.com>
2023-12-29 05:41:57 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/nav"
+
+
+function App() {
+
+  return (
+    <h1 className="text-3xl">
+      <Nav/>
+  </h1>
+  )
+}
+
+export default App
---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+
+
+const main = () => {
+  return (
+    <>
+    <div>
+        
+    </div>
+    </>
+    )
+}
+
+export default main
